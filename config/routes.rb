cf5b5a3ad28a43cb537d6fb62ba0c983fb3d0c4b
Tinh Thach Hinh <htthach@yahoo.com>
2017-03-20 22:12:23 +0800

    customize project route for fun

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects
+  resources :projects, except: [:show]
   
+  get 'project/:id', to: 'projects#show', as: 'project_show'
 
   get 'about', to: 'pages#about'
 
