357d8d9ad40d210886a866b47e644edf3098fef5
Tinh Thach Hinh <htthach@yahoo.com>
2017-03-26 14:34:41 +0800

    add nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "My Portfolio"
+    @seo_keywords = "Hinh Tinh Thach Portfolio"
+  end
+end
