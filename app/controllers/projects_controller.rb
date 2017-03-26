1fd711294de9b0d2feac5d8f6ce9642d8fa5ba61
Tinh Thach Hinh <htthach@yahoo.com>
2017-03-26 12:20:35 +0800

    refactor strong param for project item

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project_item = Project.new(params.require(:project).permit(:title, :subtitle, :body, technologies_attributes:[:name]))
+    @project_item = Project.new(project_params)
 
     respond_to do |format|
       if @project_item.save
@@ -33,7 +33,7 @@ class ProjectsController < ApplicationController
     @project_item = Project.find(params[:id])
 
     respond_to do |format|
-      if @project_item.update(params.require(:project).permit(:title, :subtitle, :body))
+      if @project_item.update(project_params)
         format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
       else
         format.html { render :edit }
@@ -51,5 +51,15 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to projects_path, notice: 'Project was successfully removed.' }
     end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, 
+                                    :subtitle, 
+                                    :body, 
+                                    technologies_attributes:[:name]
+                                    )
   end
 end
